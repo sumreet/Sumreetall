a778c84ce9ee25495e607fd6b42727056aa3bea1
sumreet <sumreet.randhawa111@gmail.com>
2021-09-23 20:45:00 +0530

    Initial commit

---
+++ funcfile.py
@@ -0,0 +1,20 @@
+Growcer = ["Apple","Pineapple","toffess"]
+print(Growcer[2])
+numbers= [2,3,4,5,6]
+print(numbers.sort())
+#numbers.reverse()
+#numbers.append(3)
+#numbers.insert(1, 52)
+#numbers.pop()
+numbers.insert(3,25)
+numbers[2]=62
+print(numbers)
+#mutable = can change
+#immutable = cannot change
+#tp= (1,2,3)
+#print(tp)
+a=3
+b=4
+a,b=b,a
+print(a,b)
+
---
+++ python ist project.py
@@ -0,0 +1,13 @@
+mystr = "Evergreen"
+#print(len(mystr))
+#print(mystr[9:3:-1])
+
+print(mystr.endswith("green"))
+print(mystr. count("e"))
+print(mystr. capitalize())
+print(mystr. find("re"))
+print(mystr. lower())
+print(mystr. capitalize())
+print(mystr. replace("re", "ge"))
+
+
