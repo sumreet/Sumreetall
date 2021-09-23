6ced573c9919162a55686265edbb4b190df7a083
sumreet <sumreet.randhawa111@gmail.com>
2021-09-23 21:40:56 +0530

    New project

--- dictionary.py
+++ dictionary.py
@@ -3,5 +3,4 @@ d2 = {"harman":"roti","sumreet":"mutton",
       "sukhdeep": {"B":"chicken", "L":"Dal"}}
 d2["azad"]="Toffess"
 del d2["azad"]
-d3[]
 print(d2.copy())
---
+++ variables.py
@@ -0,0 +1,21 @@
+#Create a dictionry and take input from user, return the meaning of the word from the dictionary
+#a="10"
+#b="20"
+#print(a)
+#print (b)
+
+#Type function
+#x= str(10)
+#y= int(10)
+#z= float(10)
+
+#print(x)
+#print(y)
+#print(z)
+
+#Type function
+
+X= "sumreet"
+y= 10
+print(type(X))
+print(type(y))
